37756ce4f8aa6aa127ffabd6e499d7d812598541
nataly <nataly@gmail.com>
2022-09-23 18:09:47 +0200

    o

---
+++ 3/pdfCarta.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <fieldset>
+            <legend>Genera tu carta de motivacion</legend>
+        <input type="text" name="nombre" id="">
+        <input type="text" name="empresa" id="">
+        <input type="text" name="representante" id="">
+        <input type="submit" value="generar PDF">
+        </fieldset>
+    </form>
+
+</body>
+</html>
