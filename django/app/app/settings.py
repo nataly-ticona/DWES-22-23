8130e40f599f17cb015eb42ac9037d0539811f2a
Nataly <natalymt03@gmail.com>
2023-02-17 18:24:45 +0100

    django app clase money

--- django/app/app/settings.py
+++ django/app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djmoney',
     'app',
     'django_extensions',
     'polls.apps.PollsConfig',
--- django/app/polls/models.py
+++ django/app/polls/models.py
@@ -1,10 +1,13 @@
+from djmoney.models.fields import MoneyField
 from django.db import models
 
 class Product(models.Model):
     photo_product = models.ImageField(upload_to='imagen', null=True, blank=False)
     name_product = models.CharField(max_length=200)
+    price_product = MoneyField(max_digits=14, decimal_places=2, default_currency='EUR', null=True, blank=False)
     description_product = models.TextField()
     pub_date_product = models.DateTimeField()
+    #para meter dinero https://django-money.readthedocs.io/en/latest/ 
 
     def __str__(self):
         return self.name_product
